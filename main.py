482223d840e6dac01c226ed6d2c0a733a94dce8e
TheEgid <egid.f1@gmail.com>
2019-10-27 18:35:19 +0300

    Add years_after_foundation function

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def get_years_after_foundation(foundation_year):
+    return datetime.now().year - foundation_year
+
+
+def render():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template_index.html')
+    years = get_years_after_foundation(foundation_year=1920)
+    rendered_page = template.render(year=years)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+def main():
+    render()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
