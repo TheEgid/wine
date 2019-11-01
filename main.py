df06185cd8b273c9b9b4c6669c8cb5ed1e05147c
TheEgid <egid.f1@gmail.com>
2019-11-01 22:11:21 +0300

    Unpack data structures

--- main.py
+++ main.py
@@ -3,40 +3,43 @@ from datetime import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def apply_list_split(lst, delimiter):
-    split_points = [i for i, item in enumerate(lst) if delimiter in item]
+def apply_list_split(lst: list, delimiter: str) -> list:
+    split_points = [index for index, item in enumerate(lst)
+                    if delimiter in item]
     split_points.append(len(lst))
-    return [lst[i: j] for i, j in zip(split_points, split_points[1:])]
+    return [lst[j: i] for j, i in zip(split_points, split_points[1:])]
 
 
-def parse_wines_contents(filepath):
+def parse_vines_content(filepath: str) -> dict:
     with open(filepath, encoding='utf8') as f:
         lines = [row.replace('\n', '') for row in f]
 
-    temp_data = list()
+    delimiter = 'Категория'
+    raw_data_list = list()
     for line in lines:
         if ': ' in line:
             line = line.split(': ')
         elif '# ' in line:
-            line = ['Категория', line.replace('# ', '')]
+            line = [delimiter, line.replace('# ','')]
         elif 'Выгодное предложение' in line:
             line = ['Акция', line]
         else:
             line = None
         if line:
-            temp_data.append(line)
+            raw_data_list.append(line)
 
-    wines_contents = dict()
-    for elem in apply_list_split(temp_data, 'Категория'):
-        category = elem[0][1]
-        contents = elem[1:]
-        wines_contents[category] = [dict(row) for row in
-                                    apply_list_split(contents, 'Название')]
+    vines_content = dict()
+    for data in apply_list_split(raw_data_list, delimiter=delimiter):
+        _category_name, *_category_content = data
+        _, category_name = _category_name
+        splitted_content_list = apply_list_split(_category_content,
+                                                 delimiter='Название')
+        vines_content[category_name] = [dict(content) for content in
+                                        splitted_content_list]
+    return vines_content
 
-    return wines_contents
 
-
-def get_years_after_foundation():
+def get_years_after_foundation() -> int:
     foundation_year = 1920
     return datetime.now().year - foundation_year
 
@@ -45,11 +48,10 @@ def render():
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template_index.html')
-
     years = get_years_after_foundation()
-    wines_contents = parse_wines_contents(filepath='products.txt')
+    wines_content = parse_vines_content(filepath='products.txt')
 
-    rendered_page = template.render(years=years, wines_contents=wines_contents)
+    rendered_page = template.render(years=years, wines_content=wines_content)
     with open('index.html', 'w', encoding='utf8') as f:
         f.write(rendered_page)
 
